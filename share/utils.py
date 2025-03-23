c9c73bfba8e5b5fd265c87f477abbfb51151d569
shritizk <shritizk@gmail.com>
2025-03-23 21:59:15 +0000

    git commit -m "Resolved conflicts and completed cherry-pick"
    done with project

---
+++ share/utils.py
@@ -0,0 +1,65 @@
+import boto3
+from botocore.exceptions import ClientError
+
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+
+def table_checker(Table_Name):
+    try:
+        table = dynamodb.Table(Table_Name)
+        if table.table_status in ["ACTIVE", "UPDATING"]:
+            return True     
+        
+
+    except ClientError as e:
+        table_created = dynamodb.create_table(
+            TableName=Table_Name, 
+            KeySchema=[
+                {"AttributeName": "id", "KeyType": "HASH"}  # Use only 'id' as the primary key
+            ],
+            AttributeDefinitions=[
+                {"AttributeName": "id", "AttributeType": "S"}  # Define 'id' as a string type
+            ],
+            BillingMode="PAY_PER_REQUEST",
+            )
+
+        table_created.wait_until_exists()
+
+        return True
+
+    except Exception as e:
+        
+        print(e)
+        return False
+
+
+# s3 bucket creator 
+def s3_bucket(bucket_name):
+     
+    try :
+        
+        s3_client = boto3.client('s3',region_name='us-east-1')
+        
+        s3_client.head_bucket(Bucket=bucket_name)
+        
+        return True
+        
+    except ClientError as e:
+        
+        try : 
+         
+            response = s3_client.create_bucket(Bucket=bucket_name)
+            
+            print(response)
+            
+            return True 
+        
+        except Exception as e:
+            
+            print(e)
+            
+            return False
+            
+            
+        
+        
+        
--- share/views.py
+++ share/views.py
@@ -3,7 +3,10 @@ from django.contrib import messages
 
 # import 
 import datetime
+<<<<<<< HEAD
 import json
+=======
+>>>>>>> a57d859 (done with project)
 
 # hashing 
 import jwt
@@ -18,9 +21,13 @@ arn = os.getenv("arn")
 
 
 #utils 
+<<<<<<< HEAD
 from aws_service.aws_service import *
 
 
+=======
+from  .utils import  s3_bucket , table_checker
+>>>>>>> a57d859 (done with project)
 
 #dynmo db 
 import boto3
@@ -180,6 +187,7 @@ def send_file(req):
                 
                 table_checker('user')
                 
+<<<<<<< HEAD
                 # get data email
                 res = scan_dynmo({
                     "search" : "email" , 
@@ -234,6 +242,55 @@ def send_file(req):
                 
                 else : 
                     
+=======
+                table = dynamodb.Table('user')
+                
+                response = table.scan(
+                    FilterExpression="email = :email",
+                    ExpressionAttributeValues={":email": receiver_email})
+                
+                if 'Items' not in response: 
+                    
+                    return redirect('/share/')
+                
+                destination_bucket = response['Items'][0].get('id')
+            
+            # get file name from req to send 
+            file_to_send = req.POST['file_to_send']
+            
+            if not  file_to_send : 
+                
+                messages.error(req, " no file selected to send  !!")
+                            
+                return redirect('/share/') 
+            
+            # reciver data 
+            
+            sender_check = s3_bucket(user_id)
+                
+            if sender_check ==  False :
+                
+                messages.error(req, " can not perform this action right now   !!")
+                            
+                return redirect('/share/') 
+            
+            else : 
+                
+                s3_client = boto3.client('s3', region_name='us-east-1')
+                
+                sender_data = s3_client.list_objects_v2(Bucket = user_id)
+                
+                file_names = [obj["Key"] for obj in sender_data.get("Contents", [])]
+                
+                if file_names == [] : 
+                    
+                    messages.error(req, " invalid action  !!")
+                            
+                    return redirect('/share/') 
+                
+                else : 
+                    
+>>>>>>> a57d859 (done with project)
                     for i in file_names : 
                         
                         if len(i.split('+')) == 3: 
@@ -242,6 +299,7 @@ def send_file(req):
                                 file_status = i
             
             copy_source = {
+<<<<<<< HEAD
                 
                     'Bucket': user_id,
                 
@@ -249,6 +307,11 @@ def send_file(req):
                 
                 
             }
+=======
+                    'Bucket': user_id,
+                    'Key': file_status
+                }
+>>>>>>> a57d859 (done with project)
                 
                 
                 # sending logic 
@@ -270,6 +333,7 @@ def send_file(req):
                             Key=reciver_key
                             ) 
                             
+<<<<<<< HEAD
                         # logic to delete it from s3 after 15 days 
                         sqs_client = boto3.client('sqs')
                         
@@ -297,6 +361,8 @@ def send_file(req):
                             'DataType': 'String',
                             'StringValue': destination_email }})
                             
+=======
+>>>>>>> a57d859 (done with project)
                         messages.error(req, " file send  !! ")
                                     
                         return redirect('/share/') 
