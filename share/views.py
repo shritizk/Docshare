f3ac9a8ba4ce71693a85f40dc811c97e5064d983
EC2 Default User <ec2-user@ip-172-31-53-178.ec2.internal>
2025-03-30 20:09:14 +0000

    did some chamhes

--- share/views.py
+++ share/views.py
@@ -152,154 +152,97 @@ def store_files(req):
 
 
 def send_file(req):
-    
-    # jwt auth 
-    
     session_token = req.COOKIES.get('session_token')
-    
-    if session_token  : 
+    if session_token:
         try:
-            
             decode = jwt.decode(session_token, SECRET_KEY, algorithms=["HS256"])
-            
             user_id = decode.get('user_id')
-            
             receiver_email = req.POST.get('receiver_email')
-            
-            # lets first check if this user even exist if ues then continue else return a message 
-                
+
             if not receiver_email:
-                
-                return redirect('/share/') 
-                
-            else : 
-                
-                table_checker('user')
-                
-                table = dynamodb.Table('user')
-                
-                response = table.scan(
-                    FilterExpression="email = :email",
-                    ExpressionAttributeValues={":email": receiver_email})
-                
-                if 'Items' not in response: 
-                    
-                    return redirect('/share/')
-                
-                destination_bucket = response['Items'][0].get('id')
+                return redirect('/share/')
+
+            # Check if user exists
+            table_checker('user')
+            table = dynamodb.Table('user')
+            response = table.scan(
+                FilterExpression="email = :email",
+                ExpressionAttributeValues={":email": receiver_email}
+            )
+
+            if 'Items' not in response:
+                return redirect('/share/')
             
-            # get file name from req to send 
+            destination_bucket = response['Items'][0].get('id')
+
+            # Get file name to send
             file_to_send = req.POST['file_to_send']
+            if not file_to_send:
+                messages.error(req, "No file selected to send!")
+                return redirect('/share/')
             
-            if not  file_to_send : 
-                
-                messages.error(req, " no file selected to send  !!")
-                            
-                return redirect('/share/') 
-            
-            # reciver data 
-            
+            # Check sender's bucket
             sender_check = s3_bucket(user_id)
-                
-            if sender_check ==  False :
-                
-                messages.error(req, " can not perform this action right now   !!")
-                            
-                return redirect('/share/') 
-            
-            else : 
-                
-                s3_client = boto3.client('s3', region_name='us-east-1')
-                
-                sender_data = s3_client.list_objects_v2(Bucket = user_id)
-                
-                file_names = [obj["Key"] for obj in sender_data.get("Contents", [])]
-                
-                if file_names == [] : 
-                    
-                    messages.error(req, " invalid action  !!")
-                            
-                    return redirect('/share/') 
-                
-                else : 
-                    
-                    for i in file_names : 
-                        
-                        if len(i.split('+')) == 3: 
-                            if i.split('+')[-1] == file_to_send :
-                                
-                                file_status = i
-            
-            copy_source = {
-                    'Bucket': user_id,
-                    'Key': file_status
-                }
-                
-                
-                # sending logic 
-                
-                
-            try : 
-                 
-                # create destination bucket 
-                
+            if sender_check == False:
+                messages.error(req, "Cannot perform this action right now!")
+                return redirect('/share/')
+
+            # Get sender's file names from S3
+            s3_client = boto3.client('s3', region_name='us-east-1')  # Ensure region_name is specified
+            sender_data = s3_client.list_objects_v2(Bucket=user_id)
+            file_names = [obj["Key"] for obj in sender_data.get("Contents", [])]
+
+            if not file_names:
+                messages.error(req, "No files available in your bucket!")
+                return redirect('/share/')
+
+            file_status = None
+            for i in file_names:
+                if len(i.split('+')) == 3:
+                    if i.split('+')[-1] == file_to_send:
+                        file_status = i
+                        break
+
+            if not file_status:
+                messages.error(req, "File not found in your bucket!")
+                return redirect('/share/')
+
+            # Create copy source
+            copy_source = {'Bucket': user_id, 'Key': file_status}
+
+            # Sending logic
+            try:
                 destination_check = s3_bucket(destination_bucket)
-                               
-                if destination_check : 
-                            
-                        reciver_key = f"{destination_bucket}+recived_from=+{copy_source.get('Bucket')}+{file_status}"
-                                
-                        s3_client.copy_object(
-                            Bucket=destination_bucket,
-                            CopySource=copy_source,
-                            Key=reciver_key
-                            ) 
-                        
-                        sqs_client = boto3.client('sqs', region_name='us-east-1')
-                         
-                        sqs_client = boto3.client('sqs')
-                        sqs_client.send_message(
-                                QueueUrl=QueueUrl,
-                                MessageBody=json.dumps({
-                                        'file_name': reciver_key,
-                                        'bucket': destination_bucket,
-                                        'destination_email': receiver_email
-                                    }),
-                                    DelaySeconds=900
-                                )    
+                if destination_check:
+                    reciver_key = f"{destination_bucket}+recived_from=+{copy_source.get('Bucket')}+{file_status}"
+                    s3_client.copy_object(
+                        Bucket=destination_bucket,
+                        CopySource=copy_source,
+                        Key=reciver_key
+                    )
 
-                        messages.error(req, " file send  !! ")
-                                    
-                        return redirect('/share/') 
-                    
-                else : 
-                        
-                    messages.error(req, " con't reach this user at  this time  !! ")
-                                    
+                    # Send message to SQS
+                    sqs_client = boto3.client('sqs', region_name='us-east-1')  # Ensure region_name is specified
+                    sqs_client.send_message(
+                        QueueUrl=QueueUrl,
+                        MessageBody=json.dumps({
+                            'file_name': reciver_key,
+                            'bucket': destination_bucket,
+                            'destination_email': receiver_email
+                        }),
+                        DelaySeconds=900
+                    )
+
+                    messages.error(req, "File sent successfully!")
                     return redirect('/share/')
-                    
-                
-            except Exception as e : 
-                    
-                    print(e)
-                    
-                    messages.error(req, " something went wrong , cant send file right now . Try again later  !! ")
-                  
-                    return redirect('/share/') 
-        
+                else:
+                    messages.error(req, "Can't reach this user at this time!")
+                    return redirect('/share/')
+            except Exception as e:
+                logger.error(f"Error during file sending: {str(e)}")
+                messages.error(req, "Something went wrong, can't send file right now. Try again later.")
+                return redirect('/share/')
         except jwt.ExpiredSignatureError:
-            
-            return redirect(req,'/') 
-    
+            return redirect('/')
         except jwt.InvalidTokenError:
-    
-            return redirect(req,'/')         
-    
-   
-    
-    
-    
- 
- 
-
-
+            return redirect('/')
